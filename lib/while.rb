3e73b52a167c8f0fe9dc563dd6d03ccb9a5e21b2
Amanda Contreras <amandaben21@aol.com>
2020-02-02 19:37:14 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,5 +1,5 @@
-def using_while
-while using_while < 10
+def levitation_force
+while levitation_force < 10
 	
 	puts"Wingardium Leviosa" while the levitation force is incremented to 10 do
 	counter += 1
